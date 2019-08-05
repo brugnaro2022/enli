71cd06eb44596258646f99bee47605bbda3281ef
marcos <marcosrodriguesbrugnaro@gmail.com>
2019-08-05 09:08:30 -0300

    Users validations and test

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+const mail = `${Date.now()}@mail.com`;
+
 test('Deve listar todos os usuários', () => {
   return request(app).get('/users')
     .then((res) => {
@@ -11,11 +13,46 @@ test('Deve listar todos os usuários', () => {
 });
 
 test('Deve inserir usuario com sucesso', () => {
-  const mail = `${Date.now()}@mail.com`;
   return request(app).post('/users')
     .send({ name: 'Walter Mitty', mail, password: '123456' })
     .then((res) => {
       expect(res.status).toBe(201);
       expect(res.body.name).toBe('Walter Mitty');
+    });
+});
+
+test('Não deve inserir usuário sem nome', () => {
+  return request(app).post('/users')
+    .send({ mail: 'test@mail.com', password: '123456' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Nome é um atributo obrigatório');
+    });
+});
+
+test('Não deve inserir usuário sem email', async () => {
+  const result = await request(app).post('/users')
+    .send({ name: 'Walter Mitty', password: '123456' });
+  expect(result.status).toBe(400);
+  expect(result.body.error).toBe('Email é um atributo obrigatório');
+});
+
+test('Não deve inserir usuário sem senha', (done) => {
+  request(app).post('/users')
+    .send({ name: 'Walter Mitty', mail: 'test@mail.com' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Senha é um atributo obrigatório');
+      done();
+    })
+    .catch(err => done.fail(err));
+});
+
+test('Não deve inserir usuário com email existente', () => {
+  return request(app).post('/users')
+    .send({ name: 'Walter Mitty', mail, password: '123456' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Já existe usuário com este email');
     });
 });
