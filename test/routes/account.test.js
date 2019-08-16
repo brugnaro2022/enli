b3ad90b106bb075eb7922260d9fa329724d7068d
marcos <psicodelicoarts@hotmail.com>
2019-08-16 10:26:13 -0300

    Find all and find by id accounts.

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -17,3 +17,24 @@ test('Deve inserir uma conta com sucesso', () => {
       expect(result.body.name).toBe('Acc #1');
     });
 });
+
+test('Deve listar todas as contas', () => {
+  return app.db('accounts')
+    .insert({ name: 'Acc list', user_id: user.id })
+    .then(() => request(app).get(MAIN_ROUTE))
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+});
+
+test('Deve retornar uma conta por Id', () => {
+  return app.db('accounts')
+    .insert({ name: 'Acc By Id', user_id: user.id }, ['id'])
+    .then(acc => request(app).get(`${MAIN_ROUTE}/${acc[0].id}`))
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.name).toBe('Acc By Id');
+      expect(res.body.user_id).toBe(user.id);
+    });
+});
