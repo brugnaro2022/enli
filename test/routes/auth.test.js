c5688389f07ddb6c8b8e3d7366d69638f4cd904b
marcos <psicodelicoarts@hotmail.com>
2019-09-03 22:34:16 -0300

    Email and password validation

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -8,7 +8,26 @@ test('Deve receber token ao logar', () => {
       .send({ mail, password: '123456' }))
     .then((res) => {
       expect(res.status).toBe(200);
-      console.log(res.body);
       expect(res.body).toHaveProperty('token');
+    });
+});
+
+test('Não deve autenticar usuário com senha errada', () => {
+  const mail = `${Date.now()}@mail.com`;
+  return app.services.user.save({ name: 'Walter', mail, password: '123456' })
+    .then(() => request(app).post('/auth/signin')
+      .send({ mail, password: '654321' }))
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha inválido');
+    });
+});
+
+test('Não deve autenticar usuário com email errado', () => {
+  return request(app).post('/auth/signin')
+    .send({ mail: 'naoexiste@mail.com', password: '654321' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Usuário ou senha inválido');
     });
 });
